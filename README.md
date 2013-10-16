1c09d624e3949e44d0e49a82a30766adaf3f4429
Lars Moelleken <voku@voku-online.de>
2013-10-16 10:42:41 +0200

    + sync with -> https://github.com/markomarkovic/simple-php-git-deploy

--- README.md
+++ README.md
@@ -1,29 +1,20 @@
-# Deploy
-_Automated project deployment via Git and PHP_
+# Simple PHP Git deploy script
+_Automatically deploy the code using PHP and Git._
 
 ## Requirements
 
-* `git`, `rsync`, and `tar` binaries are required on the server to which you would like to deploy.
-
-* The system user that's running PHP (e.g. `www-data`) needs to have the necessary permissions required to update files at the deployment location on the server.
-
-* If the Git repo you wish to deploy is private, the system user running PHP also needs to have a valid ssh key in order to access the remote repository.
-
-## Basic Idea
- Deployer is initialized by your GitHub or Bitbucket repo whenever said repo is updated. It then...
- 1. `git` clones your recently updated repo into a temporary directory on your server
- 1. Uses `rsync` to move new or changed files from the temporary directory to the deploy location
- 1. Cleans up after itself
-
+* `git`, `rsync`, and `tar` binaries are required on the server that's running the script (_server machine_).
+* The system user running PHP (e.g. `www-data`) needs to have the necessary access permissions for the `TMP_DIR` and `TARGET_DIR` locations on the _server machine_.
+* If the Git repo you wish to deploy is private, the system user running PHP also needs to have the right SSH keys to access the remote repository.
 
 ## Usage
 
- * Configure `deploy.php` and put it somewhere that's publicly accessible from the Internet.
+ * Configure `deploy.php` and put it somewhere that's accessible from the Internet.
  * Configure your git repository to call this script when the code is updated. The instructions for GitHub and Bitbucket are below.
 
 ### GitHub
 
- 1. Go to `https://github.com/USERNAME/REPOSITORY/settings/keys` and add your server SSH key (only needed for private repositories)
+ 1. (This step is only needed for private repositories) Go to `https://github.com/USERNAME/REPOSITORY/settings/keys` and add your server SSH key
  1. Go to `https://github.com/USERNAME/REPOSITORY/admin/hooks`
  1. Select the **WebHook URLs** service hook
  1. Enter the URL to your deployment script e.g. `http://example.com/deploy.php?sat=YourSecretAccessTokenFromDeployFile`
@@ -31,7 +22,7 @@ _Automated project deployment via Git and PHP_
 
 ### Bitbucket
 
- 1. Go to `https://bitbucket.org/USERNAME/REPOSITORY/admin/deploy-keys` and add your server SSH key (only needed for private repositories)
+ 1. (This step is only needed for private repositories) Go to `https://bitbucket.org/USERNAME/REPOSITORY/admin/deploy-keys` and add your server SSH key
  1. Go to `https://bitbucket.org/USERNAME/REPOSITORY/admin/services`
  1. Add **POST** service
  1. Enter the URL to your deployment script e.g. `http://example.com/deploy.php?sat=YourSecretAccessTokenFromDeployFile`
@@ -50,7 +41,7 @@ wget -q -O /dev/null http://example.com/deploy.php?sat=YourSecretAccessTokenFrom
 
 ## Done!
 
-Next time you push the code to the repository that has a hook enabled, it's going to trigger the `deploy.php` script which is going to pull the changes and update the code on the server machine.
+Next time you push the code to the repository that has a hook enabled, it's going to trigger the `deploy.php` script which is going to pull the changes and update the code on the _server machine_.
 
 For more info, read the source of `deploy.php`.
 
--- deploy.php
+++ deploy.php
@@ -1,284 +1,317 @@
 <?php
 /**
- * Deploy
- * Automated project deployment via Git and PHP.
+ * Simple PHP Git deploy script
  *
- * @version 1.0.8
- * @link https://github.com/markomarkovic/simple-php-git-deploy/
+ * Automatically deploy the code using PHP and Git.
  *
+ * @version 1.1.0
+ * @link    https://github.com/markomarkovic/simple-php-git-deploy/
  */
 
-//--------------------------------------//
-// REQUIRED CONFIGURATION
-// Things you really must setup...
+// =========================================[ Configuration start ]===
 
-// Protect the script from unauthorized access by using a secret access token.
-// Called with deploy.php?token=BetterChangeMeNowOrSufferTheConsequences
-$secret_access_token = "BetterChangeMeNowOrSufferTheConsequences";
+/**
+ * Protect the script from unauthorized access by using a secret access token.
+ * If it's not present in the access URL as a GET variable named `sat`
+ * e.g. deploy.php?sat=Bett...s the script is not going to deploy.
+ *
+ * @var string
+ */
+define('SECRET_ACCESS_TOKEN', 'BetterChangeMeNowOrSufferTheConsequences');
 
-// The HTTPS or SSH address of the remote GIT repository you wish to deploy
-// If the repo is private, you need to use the SSH address.
-$repo_address = "https://github.com/markomarkovic/simple-php-git-deploy.git";
+/**
+ * The address of the remote Git repository that contains the code that's being
+ * deployed.
+ * If the repository is private, you'll need to use the SSH address.
+ *
+ * @var string
+ */
+define('REMOTE_REPOSITORY', 'https://github.com/markomarkovic/simple-php-git-deploy.git');
 
-// The branch of the remote GIT repository (default is "master")
-$repo_branch = "master";
+/**
+ * The branch that's being deployed.
+ * Must be present in the remote repository.
+ *
+ * @var string
+ */
+define('BRANCH', 'master');
 
-// The location to which we'd like to deploy this code.
-// Don't forget the trailing slash!
-$target_directory = "/full-server-path-to-/my-deploy-location/";
+/**
+ * The location that the code is going to be deployed to.
+ * Don't forget the trailing slash!
+ *
+ * @var string Full path including the trailing slash
+ */
+define('TARGET_DIR', '/tmp/simple-php-git-deploy/');
 
-// Temporary directory we'll use to stage the code before the update
-// Say you have not already forgotten the trailing slash.
-$tmp_directory = "/full-server-path-to-/my-deploy-location/tmp/";
+/**
+ * Weather to delete the files that are not in the repository but are on the
+ * local (server) machine.
+ *
+ * !!! WARNING !!! This can lead to a serious loss of data if you're not
+ * careful. All files that are not in the repository are going to be deleted,
+ * except the ones defined in EXCLUDE section and the ones listed in .gitignore
+ * if EXCLUDE_GITIGNORE is set to true!
+ * BE CAREFUL!
+ *
+ * @var boolean
+ */
+define('DELETE_FILES', false);
 
-
-//--------------------------------------//
-// OPTIONAL CONFIGURATION
-// Things you really don't need to touch...
-
-// Create a backup of your current target directory prior to deploying.
-// To enable, add a full path here to the desired location.
-$backup_directory = "";
-
-// Wether to delete the files that are not in the repository but are on the local machine.
-// WARNING !!! This can lead to loss of data. If set to "true", all files not in the repository
-// will be deleted from the destination. ...except the ones defined in the next section. 
-$delete_files = false;
-
-// Number of seconds to allow for each command before timeout.
-$time_limit = 30;
-
-
-//--------------------------------------//
-// END CONFIGURATION
-// No need to edit anything below here.
-
-// Sets secret access token
-define('SECRET_ACCESS_TOKEN', $secret_access_token);
-
-// Sets the remote repository repo
-define('REMOTE_REPOSITORY', $repo_address);
-
-// Set deployment branch
-define('BRANCH', $repo_branch);
-
-// Sets target directory
-define('TARGET_DIR', $target_directory);
-
-// Sets delete file boolean
-define('DELETE_FILES', $delete_files);
-
-// Sets tmp directory
-define('TMP_DIR', $tmp_directory.md5(REMOTE_REPOSITORY).'-'.time().'/');
-
-// Sets timeout limit for each command.
-define('TIME_LIMIT', $time_limit);
-
-// Sets backup of the TARGET_DIR into BACKUP_DIR before deployment
-define('BACKUP_DIR', $backup_directory);
-
-// Sets a filename for a little version meta.
-define('VERSION_FILE', TMP_DIR.'DEPLOYED_VERSION.txt');
-
-
-/** TODO (merge into configuration at the top)
+/**
  * The directories and files that are to be excluded when updating the code.
  * Normally, these are the directories containing files that are not part of
  * code base, for example user uploads or server-specific configuration files.
  * Use rsync exclude pattern syntax for each element.
  *
  * @var serialized array of strings
- *
  */
-
 define('EXCLUDE', serialize(array(
 	'.git',
 	'webroot/uploads',
 	'app/config/database.php',
 )));
-?>
 
+/**
+ * Weather to exclude all files and directories listed in .gitignore.
+ * Only the .gitignore file in the project root directory is going to be used.
+ *
+ * @var boolean
+ */
+define('EXCLUDE_GITIGNORE', false);
+
+/**
+ * Temporary directory we'll use to stage the code before the update. If it
+ * already exists, script assumes that it contains an already cloned copy of the
+ * repository with the correct remote origin and only fetches changes instead of
+ * cloning the entire thing.
+ *
+ * @var string Full path including the trailing slash
+ */
+define('TMP_DIR', '/tmp/spgd-'.md5(REMOTE_REPOSITORY).'/');
+
+/**
+ * Weather to remove the TMP_DIR after the deployment.
+ * It's useful NOT to clean up in order to only fetch changes on the next
+ * deployment.
+ */
+define('CLEAN_UP', true);
+
+/**
+ * Output the version of the deployed code.
+ *
+ * @var string Full path to the file name
+ */
+define('VERSION_FILE', TMP_DIR.'VERSION.txt');
+
+/**
+ * Time limit for each command.
+ *
+ * @var int Time in seconds
+ */
+define('TIME_LIMIT', 30);
+
+/**
+ * OPTIONAL
+ * Backup the TARGET_DIR into BACKUP_DIR before deployment
+ *
+ * @var string Full backup directory path e.g. '/tmp/'
+ */
+define('BACKUP_DIR', false);
+
+// ===========================================[ Configuration end ]===
+
+?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Deployer - <?php echo $repo_url ;?> => <?php echo $target_directory ;?></title>
-        <style>
-            body {background: #222; color: #efefef; padding: 0 2em 4em 2em; font-size:1.1em;}
-            h2, .error {color: #c33; }
-            .header {color:#666; display: block;}
-            .prompt {color: #6be234; }
-            .command {color: #729fcf; }
-            .output {color: #999; }
-        </style>
-    </head>
-    
-    <body>
-    <pre>
-
+<head>
+	<meta charset="utf-8">
+	<title>Simple PHP Git deploy script</title>
+	<style>
+body { padding: 0 1em; background: #222; color: #fff; }
+h2, .error { color: #c33; }
+.prompt { color: #6be234; }
+.command { color: #729fcf; }
+.output { color: #999; }
+	</style>
+</head>
+<body>
 <?php
-if (!isset($_GET['token']) || $_GET['token'] !== SECRET_ACCESS_TOKEN) {
-    die('<h2>ACCESS DENIED</h2>');
+if (!isset($_GET['sat']) || $_GET['sat'] !== SECRET_ACCESS_TOKEN) {
+	die('<h2>ACCESS DENIED!</h2>');
 }
 if (SECRET_ACCESS_TOKEN === 'BetterChangeMeNowOrSufferTheConsequences') {
-    die("<h2>You're suffering the consequences!<br>Change the SECRET_ACCESS_TOKEN from it's default value!</h2>");
+	die("<h2>You're suffering the consequences!<br>Change the SECRET_ACCESS_TOKEN from it's default value!</h2>");
 }
 ?>
+<pre>
 
-<span class="header">
-     _            _             
-  __| | ___ _ __ | | ___  _   _ 
- / _` |/ _ \ '_ \| |/ _ \| | | |
-| (_| |  __/ |_) | | (_) | |_| |
- \__,_|\___| .__/|_|\___/ \__, |
-           |_|            |___/ 
-</span>
+Checking the environment ...
 
-Running Deployer as <b><?php echo trim(shell_exec('whoami')); ?></b>.
-
-Verifying your server environment&hellip;
+Running as <b><?php echo trim(shell_exec('whoami')); ?></b>.
 
 <?php
-// Check if the needed programs are available
-$binaries = array();
+// Check if the required programs are available
 foreach (array('git', 'rsync', 'tar') as $command) {
-    $path = trim(shell_exec('which '.$command));
-    if ($path == '') {
-        die(sprintf('<div class="error"><b>%s</b> not available. It need to be installed on the server for this script to work.</div>', $command));
-    } else {
-        $binaries[$command] = $path;
-        $version = explode("\n", shell_exec($path.' --version'));
-        printf('<b>%s</b> : %s'."\n"
-            , $path
-            , $version[0]
-        );
-    }
+	$path = trim(shell_exec('which '.$command));
+	if ($path == '') {
+		die(sprintf('<div class="error"><b>%s</b> not available. It needs to be installed on the server for this script to work.</div>', $command));
+	} else {
+		$version = explode("\n", shell_exec($path.' --version'));
+		printf('<b>%s</b> : %s'."\n"
+			, $path
+			, $version[0]
+		);
+	}
 }
 ?>
 
-Environment <span class="prompt">OK</span>.
+Environment OK.
 
 Deploying <?php echo REMOTE_REPOSITORY; ?> <?php echo BRANCH."\n"; ?>
-to <?php echo TARGET_DIR; ?>&hellip;
+to        <?php echo TARGET_DIR; ?> ...
 
-    <?php
-    // The commands
-    $commands = array();
+<?php
+// The commands
+$commands = array();
 
-    // === Pre-Deployment steps ===
+// ========================================[ Pre-Deployment steps ]===
 
-    // Clone the repository into the TMP_DIR
-    $commands[] = sprintf(
-        '%s clone --depth=1 --branch %s %s %s'
-        , $binaries['git']
-        , BRANCH
-        , REMOTE_REPOSITORY
-        , TMP_DIR
-    );
+if (!is_dir(TMP_DIR)) {
+	// Clone the repository into the TMP_DIR
+	$commands[] = sprintf(
+		'git clone --depth=1 --branch %s %s %s'
+		, BRANCH
+		, REMOTE_REPOSITORY
+		, TMP_DIR
+	);
+} else {
+	// TMP_DIR exists and hopefully already contains the correct remote origin
+	// so we'll fetch the changes and reset the contents.
+	$commands[] = sprintf(
+		'git fetch origin %s'
+		, BRANCH
+	);
+	$commands[] = sprintf(
+		'git --git-dir="%s.git" --work-tree="%s" reset --hard origin/%s'
+		, TMP_DIR
+		, TMP_DIR
+		, BRANCH
+	);
+}
 
-    // Update the submodules
-    $commands[] = sprintf(
-        '%s submodule update --init --recursive'
-        , $binaries['git']
-    );
+// Update the submodules
+$commands[] = sprintf(
+	'git submodule update --init --recursive'
+);
 
-    // Describe the deployed version
-    if (defined('VERSION_FILE') && VERSION_FILE !== '') {
-        $commands[] = sprintf(
-            '%s --git-dir="%s.git" --work-tree="%s" describe --always > %s'
-            , $binaries['git']
-            , TMP_DIR
-            , TMP_DIR
-            , VERSION_FILE
-        );
-    }
+// Describe the deployed version
+if (defined('VERSION_FILE') && VERSION_FILE !== '') {
+	$commands[] = sprintf(
+		'git --git-dir="%s.git" --work-tree="%s" describe --always > %s'
+		, TMP_DIR
+		, TMP_DIR
+		, VERSION_FILE
+	);
+}
 
-    // Backup the TARGET_DIR
-    if (defined('BACKUP_DIR') && BACKUP_DIR !== false && is_dir(BACKUP_DIR)) {
-        $commands[] = sprintf(
-            '%s czf %s/%s-%s-%s.tar.gz %s*'
-            , $binaries['tar']
-            , BACKUP_DIR
-            , basename(TARGET_DIR)
-            , md5(TARGET_DIR)
-            , date('YmdHis')
-            , TARGET_DIR // We're backing up this directory into BACKUP_DIR
-        );
-    }
+// Backup the TARGET_DIR
+if (defined('BACKUP_DIR') && BACKUP_DIR !== false && is_dir(BACKUP_DIR)) {
+	$commands[] = sprintf(
+		'tar czf %s/%s-%s-%s.tar.gz %s*'
+		, BACKUP_DIR
+		, basename(TARGET_DIR)
+		, md5(TARGET_DIR)
+		, date('YmdHis')
+		, TARGET_DIR // We're backing up this directory into BACKUP_DIR
+	);
+}
 
-    // === Deployment ===
+// ==================================================[ Deployment ]===
 
-    // Compile exclude parameters
-    $exclude = '';
-    foreach (unserialize(EXCLUDE) as $exc) {
-        $exclude .= ' --exclude='.$exc;
-    }
-    // Deployment command
-    $commands[] = sprintf(
-        '%s -rltgoDzv %s %s %s %s'
-        , $binaries['rsync']
-        , TMP_DIR
-        , TARGET_DIR
-        , (DELETE_FILES) ? '--delete-after' : ''
-        , $exclude
-    );
+// Compile exclude parameters
+$exclude = '';
+foreach (unserialize(EXCLUDE) as $exc) {
+	$exclude .= ' --exclude='.$exc;
+}
+if (EXCLUDE_GITIGNORE) {
+	// rsync looks in the TARGET directory for the .gitignore file.
+	// This works because we're using --delete-after so the current .gitignore
+	// has already been copied to the target before the delete starts.
+	$exclude .= " --filter=':- .gitignore'";
+}
+// Deployment command
+$commands[] = sprintf(
+	'rsync -rltgoDzv %s %s %s %s'
+	, TMP_DIR
+	, TARGET_DIR
+	, (DELETE_FILES) ? '--delete-after' : ''
+	, $exclude
+);
 
-    // === Post-Deployment steps ===
+// =======================================[ Post-Deployment steps ]===
 
-    // Remove the TMP_DIR
-    $commands['cleanup'] = sprintf(
-        'rm -rf %s'
-        , TMP_DIR
-    );
+// Remove the TMP_DIR (depends on CLEAN_UP)
+if (CLEAN_UP) {
+	$commands['cleanup'] = sprintf(
+		'rm -rf %s'
+		, TMP_DIR
+	);
+}
 
-    // === Run the command steps ===
+// =======================================[ Run the command steps ]===
 
-    foreach ($commands as $command) {
-        set_time_limit(TIME_LIMIT); // Reset the time limit for each command
-        if (file_exists(TMP_DIR) && is_dir(TMP_DIR)) {
-            chdir(TMP_DIR); // Ensure that we're in the right directory
-        }
-        $tmp = array();
-        exec($command.' 2>&1', $tmp, $return_code); // Execute the command
-        // Output the result
-        printf('
+foreach ($commands as $command) {
+	set_time_limit(TIME_LIMIT); // Reset the time limit for each command
+	if (file_exists(TMP_DIR) && is_dir(TMP_DIR)) {
+		chdir(TMP_DIR); // Ensure that we're in the right directory
+	}
+	$tmp = array();
+	exec($command.' 2>&1', $tmp, $return_code); // Execute the command
+	// Output the result
+	printf('
 <span class="prompt">$</span> <span class="command">%s</span>
 <div class="output">%s</div>
 '
-            , htmlentities(trim($command))
-            , htmlentities(trim(implode("\n", $tmp)))
-        );
-        flush(); // Try to output everything as it happens
+		, htmlentities(trim($command))
+		, htmlentities(trim(implode("\n", $tmp)))
+	);
+	flush(); // Try to output everything as it happens
 
-        // Error handling and cleanup
-        if ($return_code !== 0) {
-            $tmp = shell_exec($commands['cleanup']);
-            printf('
+	// Error handling and cleanup
+	if ($return_code !== 0) {
+		printf('
 <div class="error">
-Sorry, errors were encountered.
+Error encountered!
 Stopping the script to prevent possible data loss.
-Please go see what things look like in $target_directory.
+CHECK THE DATA IN YOUR TARGET DIR!
 </div>
+'
+		);
+		if (CLEAN_UP) {
+			$tmp = shell_exec($commands['cleanup']);
+			printf('
 
-Cleaning up temporary files&hellip;
+
+Cleaning up temporary files ...
 
 <span class="prompt">$</span> <span class="command">%s</span>
 <div class="output">%s</div>
 '
-                , htmlentities(trim($commands['cleanup']))
-                , htmlentities(trim($tmp))
-            );
-            error_log(sprintf(
-                'Deployment error! %s'
-                , __FILE__
-            ));
-            break;
-        }
-    }
-    ?>
+				, htmlentities(trim($commands['cleanup']))
+				, htmlentities(trim($tmp))
+			);
+		}
+		error_log(sprintf(
+			'Deployment error! %s'
+			, __FILE__
+		));
+		break;
+	}
+}
+?>
 
 Done.
-
 </pre>
 </body>
 </html>
